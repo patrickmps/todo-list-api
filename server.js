984bb9b07063a5a9cded8c27b2213a657887fee4
Patrick Mota <58093259+patrickmps@users.noreply.github.com>
2024-04-15 14:35:33 -0300

    feat: add cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const cors = require('cors')
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
@@ -7,6 +8,7 @@ const middlewares = jsonServer.defaults();
 const PORT = process.env.PORT || 3000;
 
 server.use(middlewares);
+server.use(cors());
 server.use(router);
 server.listen(PORT, () => {
 	console.log(`JSON Server is running on PORT ${PORT}`);
