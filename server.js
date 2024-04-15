2323306110d60112d06db44608f57f83c5fa0564
Patrick Mota <58093259+patrickmps@users.noreply.github.com>
2024-04-15 19:17:26 -0300

    fix: enable write operations

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const cors = require('cors')
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router(db);
 const middlewares = jsonServer.defaults();
 
 const PORT = process.env.PORT || 3000;
